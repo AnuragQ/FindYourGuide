ca004719118aabb116e67c1694f6f53f68d68bfa
noorraza <noor.raza1998@gmail.com>
2024-02-25 21:06:44 -0500

    initial setup for signup signin forgotpassword

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('offerings/<int:pk>/', views.offering_detail, name='offering_detail'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Offering
+from django.contrib.auth.views import LoginView, PasswordResetView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 def index(request):
     return render(request, 'main_app/index.html')
@@ -7,3 +11,14 @@ def index(request):
 def offering_detail(request, pk):
     offering = get_object_or_404(Offering, pk=pk)
     return render(request, 'main_app/offering_detail.html', {'offering': offering})
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'main_app/signup.html'  # Create this template in your 'templates/main_app/' folder
+
+class CustomLoginView(LoginView):
+    template_name = 'main_app/login.html'  # Create this template in your 'templates/main_app/' folder
+
+class CustomPasswordResetView(PasswordResetView):
+    template_name = 'main_app/password_reset.html'  # Create this template in your 'templates/main_app/' folder
