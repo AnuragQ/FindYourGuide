00bc18b4f42099dfa24e662eb27f2d2434faa871
noorraza <noor.raza1998@gmail.com>
2024-02-28 02:44:41 -0500

    Changes in styling_adding login logout signup button

--- main_app/views.py
+++ main_app/views.py
@@ -11,14 +11,3 @@ def index(request):
 def offering_detail(request, pk):
     offering = get_object_or_404(Offering, pk=pk)
     return render(request, 'main_app/offering_detail.html', {'offering': offering})
-
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'main_app/signup.html'  # Create this template in your 'templates/main_app/' folder
-
-class CustomLoginView(LoginView):
-    template_name = 'main_app/login.html'  # Create this template in your 'templates/main_app/' folder
-
-class CustomPasswordResetView(PasswordResetView):
-    template_name = 'main_app/password_reset.html'  # Create this template in your 'templates/main_app/' folder
