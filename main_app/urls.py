a713c06f77f3f659bd025a8013256f5d5d2e3fa9
noorraza <noor.raza1998@gmail.com>
2024-03-14 21:31:25 -0400

    Login functionality added

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('offerings/<int:pk>/', views.offering_detail, name='offering_detail'),
     path('payment/', include('payment_app.urls', namespace='payment_app')),
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', success_url='/'), name='login'),  # URL pattern for login
     path('logout/', views.logout_view, name='logout'),  # URL pattern for logout
  ]
