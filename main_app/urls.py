1faddca173096fcb489ab1764ebe18604d2f044a
noorraza <noor.raza1998@gmail.com>
2024-03-14 20:44:09 -0400

    SignUp done without designing

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from django.contrib import admin
 from django.urls import include
 from . import views
+from .views import SignUpView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('offerings/<int:pk>/', views.offering_detail, name='offering_detail'),
     path('payment/', include('payment_app.urls', namespace='payment_app')),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Offering
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomSignUpForm
 
 def index(request):
     return render(request, 'main_app/index.html')
@@ -7,3 +11,14 @@ def index(request):
 def offering_detail(request, pk):
     offering = get_object_or_404(Offering, pk=pk)
     return render(request, 'main_app/offering_detail.html', {'offering': offering})
+
+class SignUpView(CreateView):
+    form_class = CustomSignUpForm
+    success_url = reverse_lazy('index')  # Redirect to homepage upon successful signup
+    template_name = 'registration/signup.html'  # Path to the template for sign up form
+
+    def form_valid(self, form):
+        # Save the user and return the super class's form_valid method
+        response = super().form_valid(form)
+        # Additional actions after successful form submission, if any
+        return response
