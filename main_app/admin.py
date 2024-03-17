2afe70382ced3b833a6c71107a844dab39294c7e
anurag <anurag.sharma.2p@gmail.com>
2024-03-17 14:57:28 -0400

    Create and Cancel Bookings

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import User, Offering, Rating, Comment
+from .models import User, Offering, Rating, Comment, Booking
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Offering)
 admin.site.register(Rating)
 admin.site.register(Comment)
+admin.site.register(Booking)
--- main_app/forms.py
+++ main_app/forms.py
@@ -1,12 +1,27 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User, Booking, Offering
+from django.forms import ModelForm
+
 
 class CustomSignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(
+        max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'password1', 'password2')
+
+
+class BookingForm(ModelForm):
+
+    class Meta:
+        model = Booking
+        fields = ['booking_start_date', 'booking_end_date', 'no_of_guests']
+        widgets = {
+            'booking_start_date': forms.DateInput(attrs={'type': 'date', 'min': '2024-02-15', 'max': '2024-04-30'}),
+            'booking_end_date': forms.DateInput(attrs={'type': 'date'})
+        }
